fe61dde5a2af4d83689407b3e9f8edf88634260b
Rakib Choudhory <mdrakib921232@gmail.com>
2026-06-08 23:12:41 +0600

    authentication added

--- src/proxy.jsx
+++ src/proxy.jsx
@@ -10,14 +10,11 @@ export async function proxy(request) {
     headers: await headers(),
   });
   const user = session?.user;
-  console.log(user,"userrr");
-  if(user){
-
-      return NextResponse.next();
-  }
-  else{
-
-      return NextResponse.redirect(new URL('/signIn', request.url))
+  console.log(user, "userrr");
+  if (user) {
+    return NextResponse.next();
+  } else {
+    return NextResponse.redirect(new URL("/signIn", request.url));
   }
 }
 
